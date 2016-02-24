327706ff3b62537eb4f43dec17a7b041cfe88453
arkafol <makaiklari@gmail.com>
2016-02-24 17:56:03 +0100

    Kiírás javírása

--- Sorozatok/sorozatok.py
+++ Sorozatok/sorozatok.py
@@ -2,7 +2,8 @@ szamok = [1, 2, 3, 4, 5, 6]
 
 for szam in szamok:
     print(szam)
-    
+print("------------")
+
 for szam in range(1, 7):
     print(szam)
     
@@ -10,9 +11,12 @@ print("------------")
 for i in range(1, 38):
     print(i * 3)
 
+print("------------")
+
 for i in range(3, 112, 3):
     print(i)
  
+print("------------")
 for i in range(20, 0, -2):
     print(i)
 
